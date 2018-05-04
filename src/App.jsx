00d3c2f338e428b4b818b98f9a5cdc259de032bb
Alex <ajollid@gmail.com>
2018-05-04 15:19:07 -0700

    Reset index

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Ticker currency="BTC" />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
