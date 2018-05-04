2cc37eccf9d9d98f62ec941811e65df662a54853
Alex Holliday <ajhollid@gmail.com>
2018-05-04 18:05:09 +0000

    Added skeleton for Ticker.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Ticker from './Ticker.jsx';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Ticker />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
---
+++ src/Ticker.jsx
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+
+class Ticker extends Component {
+  componentDidMount() {
+    const connectionOptions = {
+      'force new connection': true,
+      reconnectionAttempts: 'Infinity',
+      timeout: 10000,
+      transports: ['websocket'],
+    };
+
+    const socket = io.connect( 'https://streamer.cryptocompare.com/', connectionOptions );
+    const subscription = ['5~CCCAGG~BTC~USD', '5~CCCAGG~ETH~USD', '11~BTC', '11~ETH'];
+    socket.emit( 'SubAdd', { subs: subscription } );
+    socket.on( 'm', ( message ) => {
+      console.log( message );
+      const messageType = message.substring( 0, message.indexOf( '~' ) );
+      if ( messageType === CCC.STATIC.TYPE.CURRENTAGG ) {
+        console.log( message );
+      } else if ( messageType === CCC.STATIC.TYPE.FULLVOLUME ) {
+        console.log( message );
+      }
+    } );
+  }
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default Ticker;
