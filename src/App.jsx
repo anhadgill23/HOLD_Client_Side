bef09df668577c4509b5a28ac3601dde463bf19a
Anhad Gill <gillanhad23@gmail.com>
2018-05-04 22:03:26 +0000

    Removed the default navbar to add a simple replacement for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { NavBar } from './NavBar';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <NavBar />
         <p className="App-intro">
           To a started, edit <code>src/App.js</code> and save to reload WHAT?  REALLY.
         </p>
---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Menu } from 'semantic-ui-react'
+
+export const NavBar = () => (
+    <Menu>
+        <Menu.Menu>
+            <Menu.Item>My Crypto Tracker</Menu.Item>
+        </Menu.Menu>
+    </Menu>
+);
+
