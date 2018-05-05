50b886f120ce65663235a2057677e4e453849ee6
Morganyyu <morganyu120@hotmail.com>
2018-05-05 21:47:20 +0000

    fetching

--- src/Register.jsx
+++ src/Register.jsx
@@ -18,9 +18,46 @@ class Register extends Component {
     if ( this.state.password !== this.state.confirmPassword ) {
       this.setState( { error: true } );
     }
-    console.log(this.state);
-  }
-    
+    console.log('state is:', this.state);
+
+    // $.ajax({
+    //   url: this.props.url,
+    //   dataType: 'json',
+    //   type: 'POST',
+    //   data: this.state,
+    //   success: function(data) {
+    //     console.log('ajax success', data);
+    //     this.setState({data: data});
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.error('ajax error', this.props.url, status, err.toString());
+    //   }.bind(this)
+    // });
+//     fetch(url, {
+//   method: 'POST', // or 'PUT'
+//   body: JSON.stringify(data), // data can be `string` or {object}!
+//   headers: new Headers({
+//     'Content-Type': 'application/json'
+//   })
+// }).then(res => res.json())
+// .catch(error => console.error('Error:', error))
+// .then(response => console.log('Success:', response));
+
+
+
+    fetch("/api/register", {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            })
+          })
+          .then(function(response) {
+            console.log('response is', response)
+          })
+          .catch(error=>console.log('Error',error));
+    }
+
   render() {
     return (
       <div className="registerForm">
