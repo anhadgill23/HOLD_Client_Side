4fd85922063ae356bf533360cb2b122f1353e039
Alex Holliday <ajhollid@gmail.com>
2018-05-05 04:45:00 +0000

    Add ESlint rule for no-console, create scaffold for transaction

---
+++ src/Modules/transaction/transaction.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Ticker extends Component {
+  constructor( props ) {
+    super( props );
+    console.log( this.props );
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return ( <div /> );
+  }
+}
+
+export default Ticker;
