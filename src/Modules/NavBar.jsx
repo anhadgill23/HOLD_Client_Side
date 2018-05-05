c47be73ade7ff9c8939ec917deccffbd2c70148a
Anhad Gill <gillanhad23@gmail.com>
2018-05-05 02:12:01 +0000

    Added log-out button

--- src/Modules/NavBar.jsx
+++ src/Modules/NavBar.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
-import { Header, Icon } from 'semantic-ui-react';
+import { Header, Icon, Button } from 'semantic-ui-react';
 import logo2 from './logo2.png';
 
 class NavBar extends Component {
   render() {
     return (
       <Menu>
-        <Menu.Menu>
           <Menu.Item>
-              <img src={logo2} />My Crypto Tracker
+              <img src={logo2} /> HOLD
           </Menu.Item>
-        </Menu.Menu>
+
+          <Menu.Item>
+            <Button> Log Out</Button>
+          </Menu.Item>
       </Menu>
     );
   }
