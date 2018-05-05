dcb4428ad174132e9667996f396400e1fe1dee8f
Anhad Gill <gillanhad23@gmail.com>
2018-05-05 21:07:44 +0000

    Fixed indentation

--- src/Modules/NavBar.jsx
+++ src/Modules/NavBar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
-import { Header, Icon, Button } from 'semantic-ui-react';
+import { Menu, Button } from 'semantic-ui-react';
 import logo2 from './logo2.png';
 
 class NavBar extends Component {
   render() {
     return (
       <Menu>
-          <Menu.Item>
-              <img src={logo2} /> HOLD
-          </Menu.Item>
+        <Menu.Item>
+          <img src={logo2} alt="cross logo" /> HOLD
+        </Menu.Item>
 
-        <Menu.Item className='ui right floated button'>
-            <Button secondary> Log Out</Button>
-          </Menu.Item>
+        <Menu.Item className="ui right floated button">
+          <Button secondary> Log Out</Button>
+        </Menu.Item>
       </Menu>
     );
   }
