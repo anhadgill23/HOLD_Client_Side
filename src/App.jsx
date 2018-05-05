7e2b61af44ad39bb0fa50fe2d870a6d89df420e5
Anhad Gill <gillanhad23@gmail.com>
2018-05-05 00:18:03 +0000

    Fixing bugs in Navbar display

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { NavBar } from './Modules/NavBar.jsx';
+import { NavBar } from './Modules/Navbar'
 
 
-class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <NavBar />
+        </header>
         <p className="App-intro">
           To a started, edit <code>src/App.js</code> and save to reload WHAT?  REALLY.
         </p>
