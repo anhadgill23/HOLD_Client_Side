65f001ea821405de98ba3ffae335a135484f3437
Alex Holliday <ajhollid@gmail.com>
2018-05-05 21:48:13 +0000

    Added test layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 
 import React, { Component } from 'react';
+import { List, Grid, Header, Divider } from 'semantic-ui-react';
 import logo from './logo.svg';
+import Ticker from './Modules/ticker/Ticker.jsx';
+import Transaction from './Modules/transaction/Transaction.jsx';
+import AddCoinModal from './Modules/add_coin_modal/AddCoinModal.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To a started, edit <code>src/App.js</code> and save to reload WHAT?  REALLY.
-        </p>
+      <div style={{ padding: '2em' }}>
+        <Header size="huge" textAlign="center">Bitcoin BTC</Header>
+        <Divider hidden />
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <Ticker currency="BTC" />
+            </Grid.Column>
+            <Grid.Column width={11}>
+              <div style={{ maxWidth: '30vw', margin: 'auto' }}>
+                <List celled verticalAlign="middle">
+                  <Transaction />
+                  <Transaction />
+                  <Transaction />
+                </List>
+              </div>
+              <AddCoinModal />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
