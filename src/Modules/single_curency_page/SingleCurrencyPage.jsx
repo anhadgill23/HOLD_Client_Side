72bde3575f365d1c455e3a4a586a6bf38ae94b72
Alex Holliday <ajhollid@gmail.com>
2018-05-07 23:08:07 +0000

    Add trash icon

--- src/Modules/single_curency_page/SingleCurrencyPage.jsx
+++ src/Modules/single_curency_page/SingleCurrencyPage.jsx
@@ -58,7 +58,8 @@ class SingleCurrencyPage extends Component {
 
   render() {
     const transactions =
-    this.dummyData.map( transaction => <Transaction key={transaction.id} transaction={transaction} /> );
+    this.dummyData.map( transaction =>
+      <Transaction key={transaction.id} transaction={transaction} /> );
     return (
       <div style={{ padding: '2em' }}>
         <Header size="huge" textAlign="center">Bitcoin BTC</Header>
