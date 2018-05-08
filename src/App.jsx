39d9711b12b9e412e963bfa54ae96abb01c56485
Morganyyu <morganyu120@hotmail.com>
2018-05-08 01:29:22 +0000

    conditional rendering of logout

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ class App extends Component {
 
       <div className="App">
 
-        <NavBar />
+        <NavBar isAuthorized={this.state.isLoggedIn} />
         <Grid>
 
         <Switch>
--- src/Modules/navbar/NavBar.jsx
+++ src/Modules/navbar/NavBar.jsx
@@ -3,16 +3,18 @@ import { Menu, Button } from 'semantic-ui-react';
 import logo2 from './logo2.png';
 
 class NavBar extends Component {
+  constructor( props ){
+    super ( props )
+  }
   render() {
     return (
       <Menu>
         <Menu.Item>
           <img src={logo2} alt="cross logo" /> HOLD
         </Menu.Item>
-
-        <Menu.Item className="ui right floated button">
+        {this.props.isAuthorized && <Menu.Item className="ui right floated button">
           <Button secondary> Log Out</Button>
-        </Menu.Item>
+        </Menu.Item>}
       </Menu>
     );
   }
