a6f50f39bee225a0c03e61ebbcb1ef6a9f201e24
Alex Holliday <ajhollid@gmail.com>
2018-05-08 16:56:34 +0000

    Add reference to props in transaction.jsx

--- src/Modules/transaction/Transaction.jsx
+++ src/Modules/transaction/Transaction.jsx
@@ -16,19 +16,20 @@ class Transaction extends Component {
 
 
   render() {
-    const transactionType = this.props.transaction.buy ? 'Cost' : 'Proceeds';
-    const buyOrSell = this.props.transaction.buy ? `${this.props.transaction.symbol} Buy Price` : `${this.props.transaction.symbol} Sell Price`;
-    const amountBoughtOrSoldHeader = this.props.transaction.buy ? 'Bought' : 'Sold';
+    const { transaction } = this.props;
+    const transactionType = transaction.buy ? 'Cost' : 'Proceeds';
+    const buyOrSell = transaction.buy ? `${transaction.symbol} Buy Price` : `${transaction.symbol} Sell Price`;
+    const amountBoughtOrSoldHeader = transaction.buy ? 'Bought' : 'Sold';
     return (
 
       <List.Item>
 
         <List.Content floated="right">
 
-          <Table fixed unstackable color={this.props.transaction.buy ? 'green' : 'red'} celled>
+          <Table fixed unstackable color={transaction.buy ? 'green' : 'red'} celled>
             <Table.Header>
               <Table.Row>
-                <Table.HeaderCell textAlign="center"><Image src={this.props.transaction.image_url} size="mini" circular /></Table.HeaderCell>
+                <Table.HeaderCell textAlign="center"><Image src={transaction.image_url} size="mini" circular /></Table.HeaderCell>
                 <Table.HeaderCell>{buyOrSell}</Table.HeaderCell>
                 <Table.HeaderCell>Trading Pair</Table.HeaderCell>
                 <Table.HeaderCell>Amount {amountBoughtOrSoldHeader}</Table.HeaderCell>
@@ -37,9 +38,9 @@ class Transaction extends Component {
             <Table.Body>
               <Table.Row>
                 <Table.Cell />
-                <Table.Cell>{this.props.transaction.price}</Table.Cell>
-                <Table.Cell>{this.props.transaction.tradingPair}</Table.Cell>
-                <Table.Cell>{this.props.transaction.amount}</Table.Cell>
+                <Table.Cell>{transaction.price}</Table.Cell>
+                <Table.Cell>{transaction.tradingPair}</Table.Cell>
+                <Table.Cell>{transaction.amount}</Table.Cell>
               </Table.Row>
             </Table.Body>
             <Table.Header>
@@ -60,9 +61,9 @@ class Transaction extends Component {
                     </Button.Content>
                   </Button>
                 </Table.Cell>
-                <Table.Cell>{this.props.transaction.transactionCost}</Table.Cell>
-                <Table.Cell>{this.props.transaction.currentWorth}</Table.Cell>
-                <Table.Cell className="percentChange">{this.props.transaction.buy ? this.props.transaction.profit : ''}</Table.Cell>
+                <Table.Cell>{transaction.transactionCost}</Table.Cell>
+                <Table.Cell>{transaction.currentWorth}</Table.Cell>
+                <Table.Cell className="percentChange">{transaction.buy ? transaction.profit : ''}</Table.Cell>
               </Table.Row>
             </Table.Body>
           </Table>
