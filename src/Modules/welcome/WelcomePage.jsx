f2a1d7386d561ded9aa555cf2d84a8bcfd00b07c
Morganyyu <morganyu120@hotmail.com>
2018-05-10 21:09:14 +0000

    homepage intro

--- src/Modules/welcome/WelcomePage.jsx
+++ src/Modules/welcome/WelcomePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import { Button, Grid, Divider } from 'semantic-ui-react';
+import ReactRevealText from 'react-reveal-text';
 import Graph from '../graph/src/Graph';
 import {
   Link,
@@ -10,12 +11,34 @@ import {
 class WelcomePage extends Component {
   constructor ( props ) {
     super ( props )
+    this.state = {
+      show: false
+    }
   }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, 700);
+  }
+
 
   render() {
     return (
       <Grid.Row>
+
       <Grid.Column>
+        <div className="intro">
+          <ReactRevealText
+            text="THE FUTURE IS NOW, TRANSCEND YOUR LIMITS"
+            show={this.state.show}
+            className="my-class-name"
+            style={{color: "#079186", fontSize: "30px", lineHeight: "36px"}}
+          />
+
+        </div>
+        <br />
+        <br />
+        <br />
         <div className="buttons-container" >
           <Link to="/register">
             <Button type="button" className="register" size="huge" inverted >Register</Button>
