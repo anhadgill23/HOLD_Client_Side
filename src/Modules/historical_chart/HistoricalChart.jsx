86ee0d9eb280da250dd0c9fc4f99177eccfbcb23
Alex Holliday <ajhollid@gmail.com>
2018-05-12 22:11:12 +0000

    Use historical graph on welcome page

--- src/Modules/historical_chart/HistoricalChart.jsx
+++ src/Modules/historical_chart/HistoricalChart.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import 'chartjs-plugin-annotation';
 import moment from 'moment';
 import { Line } from 'react-chartjs-2';
 import { Header } from 'semantic-ui-react';
 import InfoBox from './InfoBox.jsx';
 
+
 class HistoricalChart extends Component {
   static roundNumber( num, places ) {
     return ( Math.round( num * 100 ) / 100 ).toFixed( places );
   }
+
   constructor( props ) {
     super( props );
     this.state = {
@@ -46,7 +49,7 @@ class HistoricalChart extends Component {
           pointHoverBackgroundColor: 'rgba(75,192,192,1)',
           pointHoverBorderColor: 'rgba(220,220,220,1)',
           pointHoverBorderWidth: 2,
-          pointRadius: 2,
+          pointRadius: 1,
           pointHitRadius: 10,
           data: pointsArray,
         },
@@ -63,6 +66,9 @@ class HistoricalChart extends Component {
       },
       legend: {
         display: false,
+      },
+      onHover( event ) {
+        console.log( event );
       },
       scales: {
         yAxes: [{
